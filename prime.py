1b9b25c481a7372c3f385b0d03835c5ebb8653d1
k2pctdn <quangkhai338@gmail.com>
2018-11-05 15:53:55 +0900

    Re-format

--- prime.py
+++ prime.py
@@ -1,17 +1,21 @@
 import math
+
+
 def prime(n):
     if n < 1:
         return False
     elif n == 2:
         return True
     else:
-        for i in range(2,int(math.sqrt(n))+1):
+        for i in range(2, int(math.sqrt(n)) + 1):
             if n % i == 0:
                 return False
         return True
 
+
 x = int(input("Please type a natural number: "))
-print('Check prime status: ',prime(x))
+
+print('Check prime status: ', prime(x))
 if prime(x):
     print(x, 'is a prime number.')
 else:
